796237d50bfe9ba7623f63e6c52cb4baba56f55c
smjuni <stephaniejuniper@hotmail.com>
2021-01-28 13:58:02 -0800

    created dashed lines, completed skill drill

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,29 +1,31 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([30.1900, -97.6687], 4);
 
-//Get data from the cities.js
-let cityData = cities;
-  
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city),
-    L.circleMarker(city.location, {
-      radius: city.population/200000,
-      color: "orange",
-      fillcolor: "orange",
-      // fillOpacity: "0.3",
-      line: 4
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
+// Coordinates for each point to be used in the polyline.
+let line = [
+  [37.621313,	-122.378955],
+  [49.847778, -124.499167],
+  [30.1900, -97.6687],
+  [44.164371,  -80.036926],
+  [40.6413111, -73.7781391]
+];
+
+// Create a polyline using the line coordinates and make the line red.s
+L.polyline(line, {
+  color: "blue",
+  weight: 4,
+  opacity: "0.5",
+  dashArray: "10, 10"
+}).addTo(map);
+
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    id:"light-v10",
     maxZoom: 18,
     accessToken: API_KEY
 });
